60f0e4a3b6e2032a2cd132ba652672bcda2656d8
Paul <pavel.kosovich2004@gmail.com>
2023-12-01 00:46:04 +0300

    2 vers

--- Задание 2.py
+++ Задание 2.py
@@ -4,12 +4,16 @@ X = np.zeros((12, 3))
 X[:, 0] = 1
 X[:, 1] = np.random.randint(15, 27, (1, 12))
 X[:, 2] = np.random.randint(60, 82, (1, 12))
-
+print("Матрица X: ")
+print(X)
 Y = np.zeros((12, 1))
 Y[:, 0] = np.random.uniform(13.5, 18.6, (1, 12))
+print("Матрица Y: ")
 print(Y)
-
 A = np.linalg.inv(X.transpose() @ X) @ (X.transpose() @ Y)
+print("Вектор оценок A: ")
+print(A)
 
 Y = X @ A
+print("Проверка:")
 print(Y)
--- Задание 3.py
+++ Задание 3.py
@@ -1,3 +1,38 @@
+import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
-df = pd.read_csv('flats.csv')
+df = pd.read_csv('test.csv')
+
+df = df.sample(n=1000)
+
+print(f"\nПропуски в значениях:"
+      f"\n{df.isnull().sum()}")
+
+plt.figure(figsize=(12,12))
+plt.subplot(1,2,1)
+df['Square'].plot(kind='box', logy=True)
+plt.title('Ящик с усами')
+
+plt.subplot(1, 2, 2)
+df['Square'].plot(kind='hist', bins=30, logy=True)
+plt.title('Гистограмма')
+
+plt.show()
+
+#5
+numeric_columns = ['Square','LifeSquare','KitchenSquare','Healthcare_1']
+df[numeric_columns] = df[numeric_columns].fillna(df[numeric_columns].mean())
+df = df[(df['Square'] > 20) & (df['Square'] < 200)]
+
+#6
+print(f"\n\tNumber of Apartments by Number of Rooms"
+          f"\n{df['Rooms'].value_counts()}")
+
+#7
+pivot_table = df.pivot_table(index='DistrictId', columns='Rooms', values='Id', aggfunc='count')
+print(f"\n\tSummury Table"
+      f"\n{pivot_table}")
+
+#8
+df.to_csv('surname.csv',index=False)
